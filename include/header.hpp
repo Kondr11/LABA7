fb28773ca55068311045dbf06fa5a2d6947e6555
Kondr11 <alexk1201@gmail.com>
2020-02-24 17:41:21 +0400

    1.0

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,16 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Kondr11
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+
+#include <iostream>
+#include <boost/log/trivial.hpp>
+
+namespace LogSetup
+{
+
+    void init();
+
+}
 
 #endif // INCLUDE_HEADER_HPP_
